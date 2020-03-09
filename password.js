3263f1b752c3a94fe3b75054bcdc51a9a7191380
Danielle Daley <danielledaley2196@gmail.com>
2020-03-08 21:17:54 -0400

    added pseudocode

---
+++ password.js
@@ -0,0 +1,6 @@
+// require inquirer
+//give option of what's in the pass word with inquirer prompts
+//diplay using console.table
+// Password Generator Class that is responsible for concatenating the character options together.
+//   Password Class is instantiated when a new password is created.
+//  App Class that handles running the actual application that will utilize Inquirer and the other classes to create the password.
